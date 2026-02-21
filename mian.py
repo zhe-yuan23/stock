4d68957c67a1f37be3e5fafd12a204803cde2a7f
yuan <219105673+zhe-yuan23@users.noreply.github.com>
2026-02-21 20:54:41 +0800

    rearrangement

--- mian.py
+++ mian.py
@@ -1,4 +1,4 @@
-import update_data
+from src import update_data
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
@@ -13,15 +13,15 @@ for stock_id in stock_ids:
 # Analyze
 today = datetime.now()
 for stock_id in stock_ids:
-    df = pd.read_csv(f"{stock_id}_revenue.csv")
+    df = pd.read_csv(f"data/{stock_id}_revenue.csv")
     df["date"] = pd.to_datetime(df["date"])
     df = df.sort_values("date")
 
     df_last_year = df[df['date'].dt.year == today.year - 1]
     last_year_revenue = df_last_year['revenue_mon(bil)'].sum()
     df_this_year = df[df["date"].dt.year == today.year] 
-    newest_yoy = df_this_year['yoy'].iat[-1]
-    esti_revenue = last_year_revenue * (1+newest_yoy) # 推估今年營收
+    newest_yoy = df_this_year['yoy%'].iat[-1]
+    esti_revenue = last_year_revenue * (1+newest_yoy/100) # 推估今年營收
     revenue_sum = df_this_year['revenue_mon(bil)'].sum()
     revenue_achie_rate = (revenue_sum/esti_revenue*100).round(2)
 
